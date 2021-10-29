631b1deb651fa47d6589b57a67c14d6436442ec6
Extordy <vic.asd.01@gmail.com>
2021-10-29 08:46:33 -0500

    28-10-2021

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -6,9 +6,10 @@ use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
-    public function index() {
+    public function index(Request $request) {
+
         return view('test',[
-            'title' => 'Curso Laravel en Platzii !!!!!'
+            'title' => $request->query($key='title',$defaul='Valor default' )
         ]);
     }
 }
